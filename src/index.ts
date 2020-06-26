da5e05580d2945e0e23227377bd03c34bc4501f7
Dror Gluska <drorgl@yahoo.com>
2020-06-26 14:01:13 +0300

    add documentation and coverage

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-// Factory returning a function that converts a value string to n-grams.
+/**
+ * Factory returning a function that converts a value string to n-grams.
+ * @param from from number of characters
+ * @param to to number of characters
+ */
 export default function nGram(from: number, to?: number) {
 	if (typeof from !== "number" || isNaN(from) || from < 1 || from === Infinity) {
 		throw new Error("`" + from + "` is not a valid argument for n-gram");
@@ -7,12 +11,15 @@ export default function nGram(from: number, to?: number) {
 	to = to || from;
 
 	if (to < from) {
-		throw new Error("to must be larger tha from");
+		throw new Error("to must be larger than from");
 	}
 
 	return grams;
 
-	// Create n-grams from a given value.
+	/**
+	 * Create n-grams from a given value.
+	 * @param value
+	 */
 	function grams(value?: string | string[]) {
 		// const nGrams: string[] | string[][] = [];
 		const nGrams = new Set<string | string[]>();
